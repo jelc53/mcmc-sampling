348b5ca515b1ef7cbed2b4c0d63aaca19e7165bb
Julian Cooper <jelc@stanford.edu>
2022-11-25 15:47:19 -0800

    Updated gibbs logic, still does not sense check

--- source/gibbs_sampling.py
+++ source/gibbs_sampling.py
@@ -9,6 +9,26 @@ from scipy.stats import invgamma
 from common import fetch_data_for_group, generate_traceplots, generate_posterior_histograms
 
 
+def sample_sigma_sq_conditional_posterior(data, theta):
+    """Sample from sigma_sq|theta[-1] conditional posterior"""
+    tau = theta[1]
+    sigma_sq = theta[0]
+    mu = np.array([theta[2], theta[3]])
+    gam = np.array([theta[4], theta[5]])
+
+    num_data_points = data.shape[0]
+    num_groups = len(data['group'].unique())
+
+    mean = []
+    for j in range(1,num_groups+1):
+        yi_j = fetch_data_for_group(data, group_id=j)
+        mean.append(np.repeat(mu.reshape(2,1), yi_j.shape[0], axis=1).T)
+    # scale = 0.5 * np.sum(data[['X1', 'X2']]-np.vstack(mean), axis=0)
+    scale = 1
+
+    return scale * invgamma.rvs(num_data_points + 2)
+
+
 def sample_tau_conditional_posterior(data, theta):
     """Sample from tau|theta[-1] conditional posterior"""
     tau = theta[1]
@@ -22,69 +42,94 @@ def sample_tau_conditional_posterior(data, theta):
 
     # compute mean, std
     yi_4 = fetch_data_for_group(data, group_id=4)
-    mean = np.sum(np.divide(yi_4 - gam, (mu - gam)))
-    std = sigma_sq / np.sum(gam - mu)
+    mean = np.array(np.divide((gam - yi_4), (mu - gam)))
+    std = (sigma_sq / np.linalg.norm(gam-mu, 2)**2) * np.eye(2)
 
-    return np.random.normal(mean, std)
+    # sampling
+    multi_dim_samples = np.array([np.random.multivariate_normal(mean[i], std) for i in range(yi_4.shape[0])])
+    multi_dim_joint_sample = np.prod(np.vstack(multi_dim_samples), axis=0)
+
+    return multi_dim_joint_sample.mean()  # is this the right way to combine?
 
 
 def sample_mu_conditional_posterior(data, theta):
     """Sample from mu|theta[-1] conditional posterior"""
+    multi_dim_samples = []
+
     tau = theta[1]
     sigma_sq = theta[0]
-    mu = np.array([theta[2], theta[3]])
+    # mu = np.array([theta[2], theta[3]])
     gam = np.array([theta[4], theta[5]])
 
     # group 1
     yi_1 = fetch_data_for_group(data, group_id=1)
-    mu_1 = yi_1
+    mean_1 = np.array(yi_1)
+    std_1 = sigma_sq * np.eye(2)
+    multi_dim_samples_1 = np.array([np.random.multivariate_normal(mean_1[i], std_1) for i in range(yi_1.shape[0])])
+    multi_dim_samples.append(multi_dim_samples_1)
 
     # group 3
     yi_3 = fetch_data_for_group(data, group_id=3)
-    mu_3 = (yi_3 - 0.5*gam) / 0.5
+    mean_3 = np.array(2*yi_3 - gam)
+    std_3 = 4*sigma_sq* np.eye(2)
+    multi_dim_samples_3 = np.array([np.random.multivariate_normal(mean_3[i], std_3) for i in range(yi_3.shape[0])])
+    multi_dim_samples.append(multi_dim_samples_3)
 
     # group 4
     yi_4 = fetch_data_for_group(data, group_id=4)
-    mu_4 = (yi_4 - 1 + tau*gam) / tau
+    mean_4 = np.array((yi_4 - (1-tau)*gam) / tau)
+    std_4 = (sigma_sq / tau**2) * np.eye(2)
+    multi_dim_samples_4 = np.array([np.random.multivariate_normal(mean_4[i], std_4) for i in range(yi_4.shape[0])])
+    multi_dim_samples.append(multi_dim_samples_4)
 
-    # compute mean, std
-    denom = 1*4 + 0.5**2*8 + tau**2*8  # TODO: not sure about this
-    mean = (np.sum(mu_1) + np.sum(mu_3) + np.sum(mu_4)) / denom
-    std = sigma_sq / denom
+    # # compute mean, std
+    # denom = 1*4 + 0.5**2*8 + tau**2*8  # TODO: not sure about this
+    # mean = (np.sum(mu_1) + np.sum(mu_3) + np.sum(mu_4)) / denom
+    # std = sigma_sq / denom
 
-    return np.random.normal(mean, std)
+    return np.prod(np.vstack(multi_dim_samples), axis=0)
 
 
 def sample_gam_conditional_posterior(data, theta):
     """Sample from gam|theta[-1] conditional posterior"""
+    multi_dim_samples = []
+
     tau = theta[1]
     sigma_sq = theta[0]
     mu = np.array([theta[2], theta[3]])
-    gam = np.array([theta[4], theta[5]])
+    # gam = np.array([theta[4], theta[5]])
 
     # group 2
     yi_2 = fetch_data_for_group(data, group_id=2)
-    gam_2 = yi_2
+    mean_2 = np.array(yi_2)
+    std_2 = sigma_sq * np.eye(2)
+    multi_dim_samples_2 = np.array([np.random.multivariate_normal(mean_2[i], std_2) for i in range(yi_2.shape[0])])
+    multi_dim_samples.append(multi_dim_samples_2)
 
     # group 3
     yi_3 = fetch_data_for_group(data, group_id=3)
-    gam_3 = (yi_3 - 0.5*mu) / 0.5
+    mean_3 = np.array(2*yi_3 - mu)
+    std_3 = 4*sigma_sq * np.eye(2)
+    multi_dim_samples_3 = np.array([np.random.multivariate_normal(mean_3[i], std_3) for i in range(yi_3.shape[0])])
+    multi_dim_samples.append(multi_dim_samples_3)
 
     # group 4
     yi_4 = fetch_data_for_group(data, group_id=4)
-    gam_4 = (yi_4 - tau*mu) / (1 - tau)
+    mean_4 = np.array((yi_4 - tau*mu) / (1 - tau))
+    std_4 = (sigma_sq / (1-tau)**2) * np.eye(2)
+    multi_dim_samples_4 = np.array([np.random.multivariate_normal(mean_4[i], std_4) for i in range(yi_4.shape[0])])
+    multi_dim_samples.append(multi_dim_samples_4)
 
-    # compute mean, std
-    denom = 1*4 + (0.5-1)**2*8 + (tau-1)**2*8  # TODO: not sure about this
-    mean = (np.sum(gam_2) + np.sum(gam_3) + np.sum(gam_4)) / denom
-    std = sigma_sq / denom
+    # # compute mean, std
+    # denom = 1*4 + (0.5-1)**2*8 + (tau-1)**2*8  # TODO: not sure about this
+    # mean = (np.sum(gam_2) + np.sum(gam_3) + np.sum(gam_4)) / denom
+    # std = sigma_sq / denom
 
-    return np.random.normal(mean, std)
+    return np.prod(np.vstack(multi_dim_samples), axis=0)
 
 
 def gibbs_sampling(data, n_samples):
     """Implement MH sampling methodology"""
-    num_data_points = data.shape[0]
     current = [1, 0.5, 0, 0, 0, 0]
     samples = [current]
 
@@ -95,7 +140,7 @@ def gibbs_sampling(data, n_samples):
 
         if idx == 0:
             # sample sigma_sq from invgamma(n+2)
-            sigma_sq_proposal = invgamma.rvs(num_data_points + 2)
+            sigma_sq_proposal = sample_sigma_sq_conditional_posterior(data, current)
             current[0] = sigma_sq_proposal
 
         elif idx == 1:
@@ -113,6 +158,7 @@ def gibbs_sampling(data, n_samples):
             gam_proposal = sample_gam_conditional_posterior(data, current)
             current[4:6] = gam_proposal
 
+        print(current)
         samples.append(current)
 
         if it % 20 == 0:
