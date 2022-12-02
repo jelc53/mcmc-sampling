Fix hmc algorithm, tau still wide variance

source/common.py
@@ -63,6 +63,15 @@ def generate_posterior_histograms(samples, prefix=''):
     #     plt.show(); plt.close()
 
 
+def log_normal(x, mu=0, sigma=1):
+    """Helper function to query pdf from a log std normal"""
+    mu = np.ones_like(x) * mu
+    numerator = np.exp(-1*((x-mu)**2)/(2*sigma**2))
+    denominator = sigma * np.sqrt(2*np.pi)
+
+    return np.sum(np.log(numerator/denominator))
+
+
 def negative_log_prob(data, theta):
     """Compute negative log probability evaluated at a given theta"""
     n, k = data[['X1', 'X2']].shape
@@ -126,10 +135,10 @@ def fetch_param_for_group(theta, group_id):
 def center_data(mu, gam, tau, data):
     """Apply hierarchical logic to center data given theta"""
 
-    means_a = np.repeat(mu, 4).reshape(-1,2)
-    means_b = np.repeat(gam, 4).reshape(-1,2)
-    means_c = 0.5 * np.repeat(mu, 8).reshape(-1,2) + 0.5 * np.repeat(gam, 8).reshape(-1,2)
-    means_d = tau * np.repeat(mu, 8).reshape(-1,2) + (1 - tau) * np.repeat(gam, 8).reshape(-1,2)
+    means_a = np.repeat(mu, 4, axis=0)
+    means_b = np.repeat(gam, 4, axis=0)
+    means_c = 0.5 * np.repeat(mu, 8, axis=0) + 0.5 * np.repeat(gam, 8, axis=0)
+    means_d = tau * np.repeat(mu, 8, axis=0) + (1 - tau) * np.repeat(gam, 8, axis=0)
     means = np.vstack((means_a, means_b, means_c, means_d))
     centered_data = data[['X1', 'X2']] - means
 

source/gibbs_sampling.py
@@ -43,8 +43,10 @@ def sample_tau_conditional_posterior(data, theta):
     std = (sigma_sq / (n_4*np.linalg.norm(gam-mu, 2)**2))
 
     # sample truncated normal
-    a, b = (0 - mean) / std, (1 - mean) / std
+    bounds = (0, 1)
+    a, b = (bounds[0] - mean) / std, (bounds[1] - mean) / std
     dist = truncnorm(a, b, loc=mean, scale=std)
+    print(mean, std)
 
     return dist.rvs()
 

source/ham_monte_carlo.py
@@ -6,81 +6,88 @@ import numpy as np
 import pandas as pd
 import scipy.stats as st
 
-from scipy.stats import multivariate_normal
-from common import center_data, joint_posterior_density, negative_log_prob
-from common import fetch_data_for_group, fetch_param_for_group
+from common import center_data, negative_log_prob, joint_posterior_density
 from common import generate_traceplots, generate_posterior_histograms
 
 
 def dVdq(data, theta):
-    """xx"""
-    return 0
+    """Compute gradient wrt theta"""
+    X = np.array(data[['X1', 'X2']])
+    # t = np.array(data[['group']]).flatten()
+    n, k = X.shape
+
+    sigma_sq, tau, mu1, mu2, gam1, gam2 = theta
+    mu = np.array([mu1, mu2])
+    gam = np.array([gam1, gam2])
+
+    centered_data = np.array(center_data(mu.reshape(1,-1), gam.reshape(1,-1), tau, data))
+    sum_sq_cd = np.trace(centered_data @ centered_data.T)
+    grad = np.zeros_like(theta)
+
+    # gradient wrt sigma_sq
+    grad[0] = (n + 1) / sigma_sq - (1/(2*sigma_sq**2))*sum_sq_cd  # np.sum(np.power(np.linalg.norm(centered_data, ord=2, axis=1), 2))
+
+    # gradient wrt tau
+    grad[1] = (1/sigma_sq) * np.sum(centered_data[16:] @ (gam - mu))
+
+    # gradient wrt mu
+    grad[2:4] = -(np.sum((centered_data[0:4]), axis=0) +
+                  np.sum((centered_data[8:16]) * (0.5), axis=0) +
+                  np.sum((centered_data[16:]) * (tau), axis=0)
+                  ) / sigma_sq
+
+    # gradient wrt gam
+    grad[4:6] = -(np.sum(centered_data[4:8], axis=0) +
+                  np.sum(centered_data[8:16] * (0.5), axis=0) +
+                  np.sum(centered_data[16:] * (1-tau), axis=0)
+                  ) / sigma_sq
+
+    return grad
 
 
-def leapfrog(q, p, data, path_len, step_size):
+def q_update(q, p, M_mat, step_size):
+    """Helper function to update theta within bounds"""
+    q_prop = q + step_size * np.linalg.inv(M_mat) @ p
+
+    # check bounds
+    if q_prop[0] < 0:
+        p[0] = -p[0]
+        q_prop[0] = -q_prop[0]
+
+    if q_prop[1] < 0:
+        p[1] = -p[1]
+        q_prop[1] = -q_prop[1]
+
+    if q_prop[1] > 1:
+        p[1] = -p[1]
+        q_prop[1] = 1 - (q_prop[1] - 1)
+
+    return q_prop, p
+
+
+def leapfrog(q, p, data, M_mat, path_len, step_size):
     """Leapfrog integrator for Hamiltonian Monte Carlo"""
     q, p = np.copy(q), np.copy(p)  # curr_theta = q
 
-    p -= step_size * dVdq(data, q) / 2  # half step
-    for _ in range(int(path_len / step_size) - 1):
-        q += step_size * p  # whole step
-        p -= step_size * dVdq(data, q)  # whole step
-    q += step_size * p  # whole step
-    p -= step_size * dVdq(data, q) / 2  # half step
+    p -= step_size * dVdq(data, q) / 2
+    for _ in range(int(path_len / step_size)):
+        q, p = q_update(q, p, M_mat, step_size)
+        p -= step_size * dVdq(data, q)
+
+    q, p = q_update(q, p, M_mat, step_size)
+    p -= step_size * dVdq(data, q) / 2
 
     # momentum flip at end
     return q, -p
 
 
-def gradient(theta, data):
-    """
-    Evaluates the gradient of log posterior likelihood at the given value of theta.
-    Args: 
-        theta: 7 dimensional vector of parameters.
-        data: N*2 dimensional matrix of data.
-    Returns:
-        posterior: Value of posterior evaluated at the given value of theta.
-    """
-    # _n, _k = data.shape
-    # sigma_sq, tau, mu1, mu2, gam1, gam2 = theta
-    # mu = np.array([mu1, mu2])
-    # gam = np.array([gam1, gam2])
-
-    # _sigma_sq = theta[0]
-    # _sigma = np.sqrt(_sigma_sq)
-    # _lambda = 0.5
-    # _tau = theta[1]
-    # _mu = theta[2:4].reshape(1, -1)  # [1, 2]
-    # _gamma = theta[4:].reshape(1, -1)
-
-    # # Means
-    # centered_data = np.array(center_data(_mu, _gamma, _tau, data))
-    # sum_sq_cd = np.trace(centered_data @ centered_data.T)
-
-    # # gradient
-    # gradient = np.zeros_like(theta)
-
-    # # The powers are 0.5 and 1.5 because our parameter is sigma_sq while in the equation it's sigma
-    # gradient[0] = (_n + 1) / _sigma) + sum_sq_cd / (_sigma ** 4)
-    # gradient[1] = np.sum(centered_data[16:] @ (_mu - _gamma).T)
-    # gradient[2:4] = (np.sum((centered_data[0:4]), axis=0) +
-    #                  np.sum((centered_data[8:16]) * (_lambda), axis=0) +
-    #                  np.sum((centered_data[16:]) * (_tau), axis=0)
-    #                  ) / _sigma_sq
-    # gradient[4:] = (np.sum(centered_data[4:8], axis = 0) +
-    #                 np.sum(centered_data[8:16] * (1 - _lambda), axis=0) +
-    #                 np.sum(centered_data[16:] * (1 - _tau), axis=0)
-    #                 ) / _sigma_sq
-
-    return 0
-
-
-def hamiltonian_monte_carlo(data, n_samples, initial_position, step_size, path_len=1):
+def hamiltonian_monte_carlo(data, n_samples, initial_position, m, step_size, path_len=1):
     """Implement MH sampling methodology"""
     # initialization
     samples = [initial_position]
     size = (n_samples,) + initial_position.shape[:1]
-    momentum = st.norm(0, 1)
+    M_mat = np.eye(len(initial_position)) * m
+    momentum = st.norm(0, m)
 
     it = 0
     accept_count = 0
@@ -93,14 +100,21 @@ def hamiltonian_monte_carlo(data, n_samples, initial_position, step_size, path_l
             samples[-1],
             p0,
             data,
+            M_mat=M_mat,
             path_len=path_len,
             step_size=step_size,
         )
 
         # check metropolis acceptance criterion
-        curr_log_prob = negative_log_prob(data, samples[-1]) - np.sum(momentum.logpdf(p0))
-        new_log_prob = negative_log_prob(data, q_new) - np.sum(momentum.logpdf(p_new))
-        if np.log(np.random.rand()) < curr_log_prob - new_log_prob:
+        # curr_log_prob = negative_log_prob(data, samples[-1]) + (0.5/m)*np.linalg.norm(p0, 2)**2
+        # new_log_prob = negative_log_prob(data, q_new) + (0.5/m)*np.linalg.norm(p_new, 2)**2
+        # ratio = np.exp(-new_log_prob) / np.exp(-curr_log_prob)
+        curr = joint_posterior_density(data, samples[-1]) * np.exp(-(0.5/m)*np.linalg.norm(p0,2)**2)
+        prop = joint_posterior_density(data, q_new) * np.exp(-(0.5/m)*np.linalg.norm(p_new,2)**2)
+        alpha = min(1, prop/curr)
+        print(curr, prop, alpha)
+
+        if np.random.uniform(0, 1) < alpha:
             samples.append(q_new)
             accept_count += 1
         else:
@@ -122,13 +136,14 @@ if __name__ == '__main__':
     np.random.seed(42)
     n_samples = 5000
     burn_in = 200
-    path_len = 10
-    step_size = 0.1
-    initial_position = np.array([1, 0.5, 0, 0, 0, 0])
+    path_len = 1
+    m = 5
+    step_size = 0.01
+    initial_position = np.array([1., 0.5, -1.25, -0.5, -0.25, 0.3])
 
     file_path = os.path.join(os.path.pardir, 'data', infile)
     data = pd.read_csv(file_path)
 
-    samples = hamiltonian_monte_carlo(data, n_samples, initial_position, step_size, path_len)
-    generate_traceplots(samples[burn_in:], prefix='gibbs_')
-    generate_posterior_histograms(samples[burn_in:], prefix='gibbs_')
+    samples = hamiltonian_monte_carlo(data, n_samples, initial_position, m, step_size, path_len)
+    generate_traceplots(samples[burn_in:], prefix='hmc_')
+    generate_posterior_histograms(samples[burn_in:], prefix='hmc_')

source/metrpolis_hastings.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
     np.random.seed(42)
     inital_position = [1, 0.5, 0, 0, 0, 0]
     step_size = np.ones(6)*0.05
-    n_samples = 500
+    n_samples = 5000
     burn_in = 200
 
     file_path = os.path.join(os.path.pardir, 'data', infile)